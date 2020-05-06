166ce84ddad1a51ce18d9e60d8fb163037d07583
Avinash Mattar <a.k.mattar@student.tudelft.nl>
2020-05-06 21:11:18 +0200

    Added functionality to generate strong labels

--- src/tools/dataset_spec.py
+++ src/tools/dataset_spec.py
@@ -12,42 +12,50 @@ from torch.utils.data.dataset import Dataset
 
 class PriusData(Dataset):
 
-    def __init__(self, root_dir, transform=None, mode="static", class_type="coarse_class"):
+    def __init__(self, root_dir, transform=None, mode="static", class_type="coarse_class", label_type="strong_label"):
 
         self.data_dir = root_dir
-        self.mode     = mode
+        self.mode = mode
+        self.class_type = class_type
+        self.label_type = label_type
 
         # make it easier to read the csv file
-        header_names  = ["bag_name", "fine_class", "filename", "coarse_class", "location", "frame_num", "ego_motion"]
-        avail_class_t = ["coarse_class", "fine_class"]
-        use_columns   = [0,1,2,3,5,6,8]
-        avail_modes   = ["static", "driving", "all"]
+        self.header_names = ["bag_name", "fine_class", "filename", "coarse_class", "location", "frame_num",
+                             "ego_motion"]
+        self.avail_class_t = ["coarse_class", "fine_class"]
+        self.use_columns = [0, 1, 2, 3, 5, 6, 8]
+        self.avail_modes = ["static", "driving", "all"]
+        self.avail_label_t = ["strong_label", "weak_label"]
 
-        if len(header_names)!=len(use_columns):
-            raise ValueError("Number of columns({}) do not match the headers wanted({})".format(len(header_names), len(use_columns)))
+        if len(self.header_names) != len(self.use_columns):
+            raise ValueError("Number of columns({}) do not match the headers wanted({})".format(len(self.header_names),
+                                                                                                len(self.use_columns)))
 
-        self.csv_file = pd.read_csv(os.path.join(root_dir, "label_log.csv"), usecols=use_columns, header=None, names=header_names)
+        self.csv_file = pd.read_csv(os.path.join(root_dir, "label_log.csv"), usecols=self.use_columns, header=None,
+                                    names=self.header_names)
 
         # slice data as per the mode selected
         if self.mode == "static" or self.mode == "driving":
-            is_mode       = self.csv_file["ego_motion"] == self.mode
+            is_mode = self.csv_file["ego_motion"] == self.mode
             self.rel_data = self.csv_file[is_mode]
 
         elif self.mode == "all":
             self.rel_data = self.csv_file
 
         else:
-            raise ValueError("Selected mode ({}) not available. Available modes: {}".format(self.mode, avail_modes))
+            raise ValueError(
+                "Selected mode ({}) not available. Available modes: {}".format(self.mode, self.avail_modes))
 
         self.transform = transform
         self.locations = list(set(self.rel_data["location"]))
 
-        if class_type in avail_class_t:
+        if class_type in self.avail_class_t:
             self.targets = self.rel_data[class_type]
             self.classes = list(set(self.targets))
 
         else:
-            raise ValueError("Selected class type ({}) not available. Choose from: {}".format(class_type, avail_class_t))
+            raise ValueError("Selected class type ({}) not available. Choose from: {}"
+                             .format(class_type, self.avail_class_t))
 
     def __len__(self):
 
@@ -55,7 +63,7 @@ class PriusData(Dataset):
 
     def __getitem__(self, idx):
 
-        filename    = self.rel_data["filename"][idx]
+        filename = self.rel_data["filename"][idx]
         sample_type = self.rel_data["fine_class"][idx]
 
         file_location = os.path.join(*[self.data_dir, sample_type, filename])
@@ -69,11 +77,45 @@ class PriusData(Dataset):
 
         label = self.rel_data["coarse_class"][idx]
 
+        if self.label_type == "strong_label":
+            label = self.get_strong_labels(label, data.shape[1])
+
+        elif self.label_type not in self.avail_label_t:
+            raise ValueError("Selected label type ({}) not available. Choose from: {}"
+                             .format(self.label_type, self.avail_label_t))
+
         return data, label
 
+    def get_strong_labels(self, label, t_size):
+        """
+        convert weak labels to strong.
+        :param label: weak labels in the form of str
+        :return: strong labels
+        """
+        label_tensor = torch.ones([1, t_size], dtype=torch.float64)
 
+        if self.class_type == "coarse_class":
+            strong_label = torch.zeros([2, t_size], dtype=torch.float64)
 
+            if label == "positive":
+                strong_label[0, :] = strong_label[0, :] + label_tensor
 
+            else:
+                strong_label[1, :] = strong_label[1, :] + label_tensor
 
+        elif self.class_type == "fine_class":
+            strong_label = torch.zeros([4, t_size], dtype=torch.float64)
 
+            if label == "front":
+                strong_label[0, :] = strong_label[0, :] + label_tensor
 
+            elif label == "left":
+                strong_label[1, :] = strong_label[1, :] + label_tensor
+
+            elif label == "negative":
+                strong_label[2, :] = strong_label[2, :] + label_tensor
+
+            elif label == "right":
+                strong_label[3, :] = strong_label[3, :] + label_tensor
+
+        return strong_label
